3ed1af345530f68d02631f33cc0627f71b7f93c3
amar-eng <am24mo@gmail.com>
2021-07-09 00:33:18 -0400

    heroku init

--- server/server.js
+++ server/server.js
@@ -6,7 +6,28 @@ const cors = require("cors");
 const path = require("path/posix");
 
 app.use(express.json());
-app.use(cors());
+
+// Middleware
+
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://shrouded-journey-38552.heroku...",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // Warehouse route
 app.use("/warehouse", warehouseRoute);
@@ -14,12 +35,13 @@ app.use("/warehouse", warehouseRoute);
 // Inventory route
 app.use("/inventory", inventoryRoute);
 
+const path = require("path");
 if (process.env.NODE_ENV === "production") {
   // Serve ny static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
   // Handle React routing, return all requests to React App
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
