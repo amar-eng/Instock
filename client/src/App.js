fc0a1899d993709be37a4db6895641dac1cefa11
amar-eng <am24mo@gmail.com>
2023-09-01 18:21:35 -0400

    BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.scss";
-import Navbar from "./components/navbar/Navbar";
-import WarehouseAdd from "./components/WarehouseAdd/WarehouseAdd";
-import TotalInventory from "./components/TotalInventory/TotalInventory";
-import WarehouseInventory from "./components/WarehouseInventory/WarehouseInventory";
-import InventoryItem from "./components/InventoryItem/InventoryItem";
-import ItemAdd from "./components/ItemAdd/ItemAdd";
-import ItemEdit from "./components/ItemEdit/ItemEdit";
-import WarehouseEdit from "./components/WarehouseEdit/WarehouseEdit";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.scss';
+import Navbar from './components/navbar/Navbar';
+import WarehouseAdd from './components/WarehouseAdd/WarehouseAdd';
+import TotalInventory from './components/TotalInventory/TotalInventory';
+import WarehouseInventory from './components/WarehouseInventory/WarehouseInventory';
+import InventoryItem from './components/InventoryItem/InventoryItem';
+import ItemAdd from './components/ItemAdd/ItemAdd';
+import ItemEdit from './components/ItemEdit/ItemEdit';
+import WarehouseEdit from './components/WarehouseEdit/WarehouseEdit';
 
 import HomePage from './components/Homepage/HomePage';
 
 function App() {
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={HomePage} />
-          <Route path='/warehouse' exact component={HomePage} />
+          <Route path="/" exact component={HomePage} />
+          <Route path="/warehouse" exact component={HomePage} />
           <Route path="/warehouse/add" component={WarehouseAdd} />
           <Route
             path="/warehouse/edit/:warehouseid"
@@ -29,13 +29,14 @@ function App() {
             component={WarehouseInventory}
           />
           <Route path="/inventory" exact component={TotalInventory} />
-          <Route exact path='/inventory/add' component={ItemAdd} />
-          <Route path='/inventory/edit/:itemid' component={ItemEdit} />
+          <Route exact path="/inventory/add" component={ItemAdd} />
+          <Route path="/inventory/edit/:itemid" component={ItemEdit} />
           <Route path="/inventory/:itemid" component={InventoryItem} />
-          
         </Switch>
       </Router>
-      <h2 className='app-container__copyright'>© InStock Inc. All Rights Reserved.</h2>
+      <h2 className="app-container__copyright">
+        © InStock Inc. All Rights Reserved.
+      </h2>
     </div>
   );
 }
